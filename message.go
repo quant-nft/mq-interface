2911c678ae736843755da14bbdbe77c4de8dfb1f
xyths <39478565+xyths@users.noreply.github.com>
2021-09-28 10:40:06 +0800

    TelegramMessage.Preview -> DisablePreview

--- message.go
+++ message.go
@@ -1,9 +1,9 @@
 package mqi
 
 type TelegramMessage struct {
-	BotName   string  `json:"botName"`
-	ChatId    int64   `json:"chatId"`
-	Content   string  `json:"content"`
-	ParseMode *string `json:"parseMode,omitempty"`
-	Preview   *bool   `json:"preview,omitempty"`
+	BotName        string  `json:"botName"`
+	ChatId         int64   `json:"chatId"`
+	Content        string  `json:"content"`
+	ParseMode      *string `json:"parseMode,omitempty"`
+	DisablePreview *bool   `json:"disablePreview,omitempty"`
 }
